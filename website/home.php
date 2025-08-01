649d6c569a4d5e456e2ddeb2e59615ac8ace9d19
Kim <kimniklas.schlueter@gmail.com>
2025-08-01 19:35:50 +0200

    Display all animals using Bootstrap cards on home page

--- website/home.php
+++ website/home.php
@@ -9,6 +9,38 @@ $conn = db_connect();
 $myUserId = get_my_user_id_from_session();
 $myUserData = get_user_data($conn, $myUserId);
 
+$layout = "";
+
+$sql = "SELECT * FROM `animal`";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+  $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  foreach ($rows as $animal) {
+    $animalPictureUrl = $animal['picture'] ?? 'https://cdn.pixabay.com/photo/2022/02/11/00/50/golden-retriever-7006401_1280.jpg';
+
+    $layout .= "
+    <div style='width: fit-content;'>
+      <div class='card mb-4' style='max-width: 20rem; min-height: 30rem'>
+        <div class='animal-picture-container'>
+          <img src='$animalPictureUrl' class='card-img-top animal-image' alt=''>
+        </div>
+        <div class='card-body d-flex flex-column justify-content-between align-items-center'>
+          <div>
+            <h5 class='card-title'>{$animal['name']}</h5>
+            <p class='card-text'>{$animal['description']}</p>
+          </div>
+          <div>
+            <a href='#' class='btn btn-primary'>Take me home</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    ";
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -20,12 +52,29 @@ $myUserData = get_user_data($conn, $myUserId);
   <title><?= WEBSITE_TITLE ?> - Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
   <link rel="stylesheet" href="./styles/style.css">
+  <style>
+    .animal-picture-container {
+      width: 100%;
+      height: 15rem;
+      margin: auto;
+      overflow: hidden;
+    }
+
+    .animal-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  </style>
 </head>
 
 <body>
   <?php require_once './components/navbar.php'; ?>
   <div class="container mt-3 mb-5">
-    <h1>Home</h1>
+    <h1 class="mb-4">Home</h1>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 justify-content-center justify-content-md-start">
+      <?= $layout ?>
+    </div>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
